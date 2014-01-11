770e00b5350efd91516b8b351231243be63b365c
Eduardo Javier Echeverria Alvarado <echevemaster@gmail.com>
2014-01-11 04:14:13 -0430

    Rails structure

--- bbredmine.rb
+++ bbredmine.rb
@@ -9,10 +9,7 @@ require 'rest-client'
 # Redmines
 # Necesitamos un arreglo de Redmine a los que conectarnos para recuperar las peticiones
 
-class Redmine
-  include RestClient 
-  attr_accessor 'url'
-  attr_accessor 'apikey'
+class Redmine < RestClient::Resource
 end
 
-Redmine.get 'http://gomix.fedora-ve.org/issues.xml?assigned_to_id=2&key=b899118d094e085b63658425dbba65629d619333'
+# Redmine.url 'http://gomix.fedora-ve.org/issues.xml?assigned_to_id=2&key=b899118d094e085b63658425dbba65629d619333'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bbredmine::Application.config.secret_key_base = '2edf6c59fc94425bd1516f11948f1020e356b406a4e394c5b971e1dfb70214d85e491a3f7fc4f78070237b8f42bdfdf612cdea697095d33d49a1008493e80be1'
